ad62e90405b2b1d0d53d5601f7e9385d072ddefd
Sandro Volery <sandro@volery.org>
2024-10-18 10:12:08 +0200

    Fix timing issue where class isn't ready yet on newer versions of iOS

--- tiktoksslbypass.js
+++ tiktoksslbypass.js
@@ -1,23 +1,32 @@
 /*
-	This script bypasses SSL pinning that TikTok has and may change in the future.
-	This is for educational purposes only.
+  This script bypasses SSL pinning that TikTok has and may change in the future.
+  This is for educational purposes only.
 	
-	To run, run the following command:
-	frida -U -f com.zhiliaoapp.musically --no-pause -l tiktoksslbypass.js
+  To run, run the following command:
+  frida -U -f com.zhiliaoapp.musically --no-pause -l tiktoksslbypass.js
 
-	Please note this will only work on Jailbroken iOS devices. You must have frida installed prior to running this script.
+  Please note this will only work on Jailbroken iOS devices. You must have frida installed prior to running this script.
 
 */
 
-Interceptor.attach(ObjC.classes.TTHttpTask["- skipSSLCertificateError"].implementation, {
-onEnter: function (args) {
-    
-},
-onLeave: function (retval) {
-    console.log('Overriding -> TTHttpTask skipSSLCertificateError : ');
-    retval.replace(0x1)
+function waitForClass(className, callback) {
+  var interval = setInterval(function() {
+    if (ObjC.classes[className]) {
+      clearInterval(interval);
+      callback();
+    }
+  }, 100);
 }
+
+waitForClass('TTHttpTask', function() {
+  Interceptor.attach(ObjC.classes.TTHttpTask["- skipSSLCertificateError"].implementation, {
+    onEnter: function(args) {
+    },
+    onLeave: function(retval) {
+      console.log('Overriding -> TTHttpTask skipSSLCertificateError : ');
+      retval.replace(0x1);
+    }
+  });
+
+  console.log('Successfully Initialized SSL Bypass...');
 });
-
-
-console.log('Successfully Initalized SSL Bypass...');
